234bec7a55949c1f89cf5094ca4c525038eeab86
MesaZapata <114545267+MesaZapata@users.noreply.github.com>
2023-02-10 19:01:54 -0500

    Actualizar dbz.module.ts

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
 import { FormsModule } from '@angular/forms';
 import { PersonajesComponent } from './personajes/personajes.component';
-import { AgregarComponent } from './agregar/agregar.component';
+import { AgregarComponent } from './dbz/agregar/agregar.component';
 
 
 
