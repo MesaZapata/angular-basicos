62e6d8297cdf41a98dc8ee93b02321381e57c90c
Samuel Mesa <mesazapatasamuel@gmail.com>
2023-03-01 11:27:23 -0500

    commit final

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainPageComponent } from './main-page/main-page.component';
 import { FormsModule } from '@angular/forms';
+
+import { MainPageComponent } from './main-page/main-page.component';
 import { PersonajesComponent } from './personajes/personajes.component';
 import { AgregarComponent } from './agregar/agregar.component';
+
 import { DbzService } from './services/dbz.service';
-import { AgregarComponent } from './dbz/agregar/agregar.component';
 
 
 
@@ -13,14 +14,14 @@ import { AgregarComponent } from './dbz/agregar/agregar.component';
   declarations: [
     MainPageComponent,
     PersonajesComponent,
-    AgregarComponent
+    AgregarComponent,
   ],
   exports: [
-    MainPageComponent
+    MainPageComponent,
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [
     DbzService
